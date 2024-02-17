b745184caefceabe90695ad3ad02ae7ebdc8f251
pushpit07 <pushpit07@gmail.com>
2024-02-17 22:23:45 +0100

    Minimal system
    Signed-off-by: pushpit07 <pushpit07@gmail.com>

--- warehouse-admin/src/main/java/com/onlineshop/controller/MqttController.java
+++ warehouse-admin/src/main/java/com/onlineshop/controller/MqttController.java
@@ -1,6 +1,5 @@
 package com.onlineshop.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,22 +9,20 @@ import org.eclipse.paho.client.mqttv3.MqttClient;
 import org.eclipse.paho.client.mqttv3.MqttException;
 import org.eclipse.paho.client.mqttv3.MqttMessage;
 
-import com.onlineshop.MqttGateway;
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 
 @RestController
 public class MqttController {
-	@Autowired
-	MqttGateway mqtGateway;
 
 	@PostMapping("/add-item-to-db")
-	public ResponseEntity<?> publish(@RequestBody String mqttMessage){
+	public ResponseEntity<?> addItemToDb(@RequestBody String mqttMessage){
 		try {
 			JsonObject convertObject = new Gson().fromJson(mqttMessage, JsonObject.class);
-			// mqtGateway.sendToMqtt(convertObject.get("message").toString(), convertObject.get("topic").toString());
-			// return ResponseEntity.ok("Success");
 
+			// perform add to db operation
+
+			// publish event to topic
 			MqttClient client = new MqttClient("tcp://mosquitto:1883", MqttClient.generateClientId());
 			client.connect();
 
